cf380b92e56f9ab6f04340e7e17f424ac56aa99f
Avatar-Arcade <renatobonafon@hotmail.com>
2022-06-15 18:19:04 -0500

    HU-01

---
+++ app/src/main/java/com/example/groupapp/AlturaUser.kt
@@ -0,0 +1,11 @@
+package com.example.groupapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlturaUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_altura_user)
+    }
+}
---
+++ app/src/main/java/com/example/groupapp/Userdata.kt
@@ -0,0 +1,11 @@
+package com.example.groupapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Userdata : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_userdata)
+    }
+}
